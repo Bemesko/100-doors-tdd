33615e89335c8cbe44b8bb0a8f464d7dab7bcb9f
3QxlmN <3QxlmN@cyber-dojo.org>
2023-07-13 10:31:27 +0000

    10 ran tests, no prediction, got amber

--- files/test_doors.py
+++ files/test_doors.py
@@ -9,3 +9,7 @@ def test_open_closed_door():
     door = Door(is_open=False)
     door.toggle_open()
     assert door.is_open is True
+    
+def test_create_list_of_doors():
+    door_list = DoorList(doors=5)
+    assert len(door_list.doors) == 5
