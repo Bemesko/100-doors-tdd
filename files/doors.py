05f86b11096e0805d640e21fbfc8621e8a4dd29a
3QxlmN <3QxlmN@cyber-dojo.org>
2023-07-13 10:28:54 +0000

    7 ran tests, no prediction, got green

--- files/doors.py
+++ files/doors.py
@@ -1,8 +1,8 @@
 class Door:
     is_open = False
-    
+
     def __init__(self, is_open: bool):
         self.is_open = is_open
-        
+
     def toggle_open(self):
         self.is_open = not self.is_open
--- files/test_doors.py
+++ files/test_doors.py
@@ -3,4 +3,4 @@ from doors import Door
 def test_close_open_door():
     door = Door(is_open=True)
     door.toggle_open()
-    assert door.is_open == False
+    assert door.is_open is False
