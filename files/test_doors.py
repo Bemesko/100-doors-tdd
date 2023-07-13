57e85eb643167547cf9a05dd5ea4671031a8056f
3QxlmN <3QxlmN@cyber-dojo.org>
2023-07-13 10:51:37 +0000

    27 ran tests, no prediction, got amber

--- files/test_doors.py
+++ files/test_doors.py
@@ -15,16 +15,19 @@ def test_create_default_door():
     assert door.is_open is False
     
 def test_create_list_of_doors():
+    door_list = None
     door_list = DoorList(doors=5)
     assert len(door_list.doors) == 5
 
 def test_all_doors_are_false():
+    door_list = None
     door_list = DoorList(doors=3)
     open_doors = sum(door_list.list_open_doors())
     
     assert open_doors == 0
     
 def test_toggle_all_doors_should_make_all_open():
+    door_list = None
     doorAmount = 3
     door_list = DoorList(doors=doorAmount)
     door_list.toggle_doors(step=1)
