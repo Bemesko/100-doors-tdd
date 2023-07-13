45a46c042c9e94638859e306551db1335c0e861c
3QxlmN <3QxlmN@cyber-dojo.org>
2023-07-13 10:36:52 +0000

    17 ran tests, no prediction, got green

--- files/doors.py
+++ files/doors.py
@@ -1,10 +1,7 @@
 class Door:
     is_open = False
-    
-    def __init__(self):
-        self.is_open = False
 
-    def __init__(self, is_open: bool):
+    def __init__(self, is_open=False):
         self.is_open = is_open
 
     def toggle_open(self):
